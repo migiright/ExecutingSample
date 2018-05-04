4793950e7e8668d2cc890aac691992f296fd01b6
migiright <migigit@yahoo.co.jp>
2018-05-04 15:19:18 +0900

    Add codes

--- ExecutingSample/Program.cs
+++ ExecutingSample/Program.cs
@@ -10,6 +10,15 @@ namespace ExecutingSample
 	{
 		static void Main(string[] args)
 		{
+			var i = new System.Diagnostics.ProcessStartInfo("a.exe") {
+				RedirectStandardOutput = true,
+				RedirectStandardInput = true,
+				UseShellExecute = false,
+			};
+			using (var p = System.Diagnostics.Process.Start(i)) {
+				p.StandardInput.WriteLine(14);
+				Console.WriteLine(p.StandardOutput.ReadToEnd());
+			}
 		}
 	}
 }
